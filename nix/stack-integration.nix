e336577349839a4a5881e50a64d82dcdcd0c6949
Sergey Yakovlev <fclaw007@gmail.com>
2023-05-24 20:35:05 +0300

    deploy

--- nix/stack-integration.nix
+++ nix/stack-integration.nix
@@ -1,15 +1,23 @@
 let
   sources = import ./sources.nix;
   pkgs = import sources.nixpkgs { };
+
+  gmp = pkgs.picom.overrideAttrs (old: {
+      name = "gmp-6.2.1";
+      src = fetchurl {
+        url = https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz;
+        sha256 = "0x2g1jqygyr5wiwg4ma1nd7w4ydpy82z9gkcv8vh2v8dn3y58v5m";
+       };
+    });
 in
 
 # See https://docs.haskellstack.org/en/stable/nix_integration/#using-a-custom-shellnix-file
 { ghc }:
 
 pkgs.haskell.lib.buildStackProject {
-  
+
   inherit ghc;
   name = "haskell-stack-nix";
   # System dependencies needed at compilation time
-  buildInputs = [ pkgs.postgresql pkgs.lzma pkgs.git pkgs.zlib pkgs.imagemagick pkgs.gmp ];
+  buildInputs = [ pkgs.postgresql pkgs.lzma pkgs.git pkgs.zlib pkgs.imagemagick gmp ];
 }
