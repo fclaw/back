3c69923b857b3fc8447177a6a85efe1ea8223b68
Sergey Yakovlev <fclaw007@gmail.com>
2023-05-24 19:43:55 +0300

    deploy

--- shell.nix
+++ shell.nix
@@ -28,8 +28,6 @@ pkgs.mkShell {
   buildInputs = [
     stack-wrapped
   ];
-  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.gmp ];
-
   # Configure the Nix path to our own `pkgs`, to ensure Stack-with-Nix uses the correct one rather than the global <nixpkgs> when looking for the right `ghc` argument to pass in `nix/stack-integration.nix`
   # See https://nixos.org/nixos/nix-pills/nix-search-paths.html for more information
   NIX_PATH = "nixpkgs=" + pkgs.path;
