871a71378209af23369d20de5f291cda314e8732
Sergey Yakovlev <fclaw007@gmail.com>
2023-05-28 22:58:36 +0300

    deploy

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-# export ekg_datadir=server/deploy/ekg
-cd server
+#!/bin/sh
 
 echo 'launch server..'
+nix-channel --update
 nix-shell --command "./bin/server --cfg_path deploy/config.yaml --path_to_katip deploy --path_to_jwk deploy --cfg_admin_storage_path deploy/admin_storage"
