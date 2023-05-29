a5dcfb232e2ba65edf4888b1a246309a4d85a8fa
Sergey Yakovlev <fclaw007@gmail.com>
2023-05-29 10:16:46 +0300

    deploy

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 echo 'launch server..'
-nix-channel --update
 nix-shell --command "./bin/server --cfg_path deploy/config.yaml --path_to_katip deploy --path_to_jwk deploy --cfg_admin_storage_path deploy/admin_storage"
