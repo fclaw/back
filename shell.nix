1539a1d53d5cb62f794768ad089689ee918c71dd
Sergey Yakovlev <fclaw007@gmail.com>
2023-05-30 13:55:14 +0300

    fix

--- shell.nix
+++ shell.nix
@@ -27,7 +27,6 @@ in
 pkgs.mkShell {
   buildInputs = [
     stack-wrapped
-    pkgs.postgresql
   ];
   # Configure the Nix path to our own `pkgs`, to ensure Stack-with-Nix uses the correct one rather than the global <nixpkgs> when looking for the right `ghc` argument to pass in `nix/stack-integration.nix`
   # See https://nixos.org/nixos/nix-pills/nix-search-paths.html for more information
