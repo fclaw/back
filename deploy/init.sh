f4cec23a9331fbbcc713de8a9b5beb357e56e9f3
Sergey Yakovlev <fclaw007@gmail.com>
2023-06-11 18:40:55 +0300

    deploy

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 echo 'launch server..'
-. /home/nix/.nix-profile/etc/profile.d/nix.sh && \ 
-  nix-shell deploy.nix --log-format bar-with-logs --verbose --command "./bin/server --cfg_path deploy/config.yaml --path_to_katip deploy --path_to_jwk deploy --cfg_admin_storage_path deploy/admin_storage --print_cfg y"
+. /home/nix/.nix-profile/etc/profile.d/nix.sh && command nix-shell ./nix/deploy.nix --log-format bar-with-logs --verbose --command "./bin/server --cfg_path deploy/config.yaml --path_to_katip deploy --path_to_jwk deploy --cfg_admin_storage_path deploy/admin_storage --print_cfg y"
